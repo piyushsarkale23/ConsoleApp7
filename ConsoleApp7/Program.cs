0de6e0e6db0cf7df25fd767efd082385eb6bb35b
piyushsarkale23 <piyushsarkale35@gmail.com>
2023-04-05 18:14:26 +0530

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    public class Program
+    {
+      static void Swap(int a,int b)
+        {
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+
+        }
+        static void main(String[] args)
+        {
+            int a = 12, b = 13;
+            Swap(a,b);
+            Console.WriteLine($"The value of a is {a} and value of b is {b}");
+        }
+
+
+    }
+}
